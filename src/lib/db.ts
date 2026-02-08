ec7f17187fa55c0e28cdd36731ceb678cb0f556e
profzen <profzzen@gmail.com>
2026-02-08 02:02:51 +0000

    site base 2 vercel deploy fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI ?? "";
 
-if (!MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Missing MONGODB_URI in environment variables");
 }
 
